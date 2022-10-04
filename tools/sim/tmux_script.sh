084a3bfaef001655671dde63b301d8f40cb090e6
Hamid Ebadi <hamid.ebadi@gmail.com>
2022-10-04 14:59:24 +0200

    TPlink access point ip address

--- tools/sim/tmux_script.sh
+++ tools/sim/tmux_script.sh
@@ -2,9 +2,9 @@
 tmux new -d -s carla-sim
 tmux send-keys "./launch_openpilot.sh  2>&1 | tee out.txt" ENTER
 tmux neww
-tmux send-keys "export ROS_MASTER_URI=http://192.168.150.140:11311 ; export ROS_IP=192.168.150.107 ; source /opt/ros/noetic/setup.bash ; . /opt/ros/noetic/setup.bash ; cd gokart_controllerx ; $*"  ENTER "python gokart.py" ENTER
+tmux send-keys "export ROS_MASTER_URI=http://192.168.0.200:11311 ; export ROS_IP=192.168.0.100 ; source /opt/ros/noetic/setup.bash ; . /opt/ros/noetic/setup.bash ; cd gokart_controllerx ; $*"  ENTER "python gokart.py" ENTER
 tmux neww
-tmux send-keys "export ROS_MASTER_URI=http://192.168.150.140:11311 ; export ROS_IP=192.168.150.107 ; source /opt/ros/noetic/setup.bash ; . /opt/ros/noetic/setup.bash ; rostopic pub Speed_Request_Speed std_msgs/Float32 8 $*" ENTER 
+tmux send-keys "export ROS_MASTER_URI=http://192.168.0.200:11311 ; export ROS_IP=192.168.150.100 ; source /opt/ros/noetic/setup.bash ; . /opt/ros/noetic/setup.bash ; rostopic pub Speed_Request_Speed std_msgs/Float32 8 $*" ENTER 
 tmux neww
 tmux send-keys "./../replay/ui.py $*" ENTER
 tmux a -t carla-sim
